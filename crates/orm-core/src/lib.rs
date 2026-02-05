4077850173ec9ec82eccab224e243e8e57843060
Matt Tsogt <mandah0628@gmail.com>
2026-02-04 23:03:37 -0600

    another commit

--- crates/orm-core/src/lib.rs
+++ crates/orm-core/src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::Seek;
+
 use sqlx::{postgres::PgPoolOptions, PgPool, Postgres, Transaction};
 use thiserror::Error;
 
@@ -8,4 +10,43 @@ pub enum OrmError {
 
     #[error("missing DATABASE_URL environment variable")]
     MissingDatabaseUrl
+}
+
+pub type OrmResult<T> = Result<T, OrmError>;
+
+#[derive(Clone)]
+pub struct Db {
+    pool: PgPool
+}
+
+pub struct Tx<'a> {
+    inner: Transaction<'a, Postgres>
+}
+
+
+impl Db {
+    pub async fn connect(database_url: &str) -> OrmResult<Self> {
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(database_url)
+            .await?;
+
+        Ok(Self{pool})
+    }
+
+    pub async fn execute(&self, sql: &str) -> OrmResult<u64> {
+        let res = sqlx::query(sql).execute(&self.pool).await?;
+        Ok(res.rows_affected())
+    }
+
+    pub async fn fetch_i64(&self, sql: &str) -> OrmResult<i64> {
+        let val: i64 = sqlx::query_scalar(sql).fetch_one(&self.pool).await?;
+        Ok(val)
+    }
+
+    pub async fn transaction<F, Fut, T>(&self, f: F) -> OrmResult<T> {
+        where
+            F: for<'a> Fn
+
+    }
 }
